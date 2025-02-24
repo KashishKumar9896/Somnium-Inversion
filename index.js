1f66e38052bc026d1bff8638a181ee81b630e4d2
KashishKumar9896 <Kashishkumar4321@gmail.com>
2025-02-25 00:07:54 +0530

    l

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("eventsRedirect").addEventListener("click", function() {
+    window.location.href = "more.html"; // Redirect to new.html
+});
+document.getElementById("pol").addEventListener("click", function() {
+    window.location.href = "more2.html"; // Redirect to new.html
+});
+document.getElementById("lol").addEventListener("click", function() {
+    window.location.href = "more1.html"; // Redirect to new.html
+});
+document.getElementById("pl").addEventListener("click", function() {
+    window.location.href = "1.html"; // Redirect to new.html
+});
+document.getElementById("v").addEventListener("click", function() {
+    window.location.href = "2.html"; // Redirect to new.html
+});
+document.getElementById("e").addEventListener("click", function() {
+    window.location.href = "3.html"; // Redirect to new.html
+});
+
+
